506372eb4606b3b602933c36406ec8964c27a0c9
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2025-09-14 13:17:03 +0200

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,53 @@
+let random = 0
+input.onGesture(Gesture.Shake, function () {
+    random = randint(1, 6)
+    if (random == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (random == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (random == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (random == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (random == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
